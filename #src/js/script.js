20fc20905af0ab3ce08d3aede77d759e5db8b6a5
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2021-09-28 00:11:39 +0300

    navigation menu complete

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict';
 
+	// ------------ navigation menu --------------------
+
+	(() => {
+
+		const hamburgerBtn = document.querySelector('.hamburger-btn'),
+			navMenu = document.querySelector('.nav-menu'),
+			closeNavBtn = navMenu.querySelector('.close-nav-menu');
+
+		hamburgerBtn.addEventListener('click', showNavMenu);
+		closeNavBtn.addEventListener('click', hideNavMenu);
+
+
+		function showNavMenu() {
+			navMenu.classList.add('open');
+			bodyScrollingToggle();
+		}
+
+		function hideNavMenu() {
+			navMenu.classList.remove('open');
+			fadeOutEffect();
+			bodyScrollingToggle();
+		}
+
+		function fadeOutEffect() {
+			document.querySelector('.fade-out-effect').classList.add('active');
+			setTimeout(() => {
+				document.querySelector('.fade-out-effect').classList.remove('active');
+			}, 300);
+		}
+
+		//  attach and event handler to document
+		document.addEventListener('click', (event) => {
+			if (event.target.classList.contains('link-item')) {
+				// make sure event.target.hash has a value before overridding default behavior
+				if (event.target.hash !== '') {
+
+					event.preventDefault();
+					const hash = event.target.hash;
+					// deactivate existing active 'section'
+					document.querySelector('.section.active').classList.add('hide');
+					document.querySelector('.section.active').classList.remove('active');
+					// activate new 'section'
+					document.querySelector(hash).classList.add('active');
+					document.querySelector(hash).classList.remove('hide');
+					// deactivate existing active navigation menu 'link-item'
+					navMenu.querySelector('.active').classList.add('outer-shadow', 'hover-in-shadow');
+					navMenu.querySelector('.active').classList.remove('active', 'inner-shadow');
+
+					if (navMenu.classList.contains('open')) {
+						// activate new navigation menu 'link-item'
+						event.target.classList.add('active', 'inner-shadow');
+						event.target.classList.remove('outer-shadow', 'hover-in-shadow');
+						// hide navigation menu
+						hideNavMenu();
+
+					} else {
+						let navItems = navMenu.querySelectorAll('.link-item');
+						navItems.forEach((item) => {
+							if (hash === item.hash) {
+								// activate new navigation menu 'link-item'
+								item.classList.add('active', 'inner-shadow');
+								item.classList.remove('outer-shadow', 'hover-in-shadow');
+							}
+						});
+
+						fadeOutEffect();
+					}
+
+
+					// add hash (#) to url
+					window.location.hash = hash;
+				}
+			}
+		});
+
+
+
+	})();
+
 
 	//------------- About section tabs -------------------
 
@@ -41,7 +120,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		let itemIndex, slideIndex, screenshots;
 
-		// filter portfolio items
+		// ----------- filter portfolio items -------------------
 		filterContainer.addEventListener('click', (event) => {
 			if (event.target.classList.contains('filter-item') && !event.target.classList.contains('active')) {
 				filterContainer.querySelector('.active').classList.remove('outer-shadow', 'active');
@@ -211,6 +290,19 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 		slider();
 
+
+	})();
+
+
+	// ---------------- hide all section except active ----------------
+	(() => {
+
+		const section = document.querySelectorAll('.section');
+		section.forEach((section) => {
+			if (!section.classList.contains('active')) {
+				section.classList.add('hide');
+			}
+		});
 
 	})();
 
