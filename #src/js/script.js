35f7e67f342d979ae4cf995ca56f250cf32884b5
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2021-09-28 00:59:26 +0300

    color skins

--- #src/js/script.js
+++ #src/js/script.js
@@ -306,4 +306,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	})();
 
+	// ----------------------- style-switcher -----------------------
+
+	const styleSwitcherToggler = document.querySelector('.style-switcher-toggler');
+
+	styleSwitcherToggler.addEventListener('click', () => {
+		document.querySelector('.style-switcher').classList.toggle('open');
+	});
+
+	// hide style-switcher on scroll
+
+	window.addEventListener('scroll', () => {
+		if (document.querySelector('.style-switcher').classList.contains('open')) {
+			document.querySelector('.style-switcher').classList.remove('open');
+		}
+	});
+
+	// ------------------------ theme colors ---------------------------
+	
+
 });
